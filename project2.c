c2d9c34b3ca3587b652ebec19ca1f498bfe4224f
bogmin <55713690+bogmin@users.noreply.github.com>
2021-12-02 14:31:49 -0600

    Create project2.c

---
+++ project2.c
@@ -0,0 +1,181 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct block {
+    int memory;     //how much memory it takes up
+    int memStart;   //starting location of memory
+    struct block *next;
+    char process[]; //process name; treat this like the key
+} block;
+
+struct block *head = NULL;
+struct block *current = NULL;
+
+char instruction[50]; //holds the instruction/values to be assigned/listed/etc. each 'fit' will perform
+//instructions as needed
+char *instructArray[200];   //array to hold lists of instructions
+//struct block* memStore[200];        //array to hold the various memory blocks
+int requests = 0;   //number of requests for blocks
+int totalMem;   //total amount of memory assigned at read
+
+void readInstructions(){
+
+}
+
+void insertFirst(char process[], int memory, int memStart){
+    struct block *link = (struct block*) malloc(sizeof(struct block));
+    link->memStart = memStart;
+    link->memory = memory;
+    strcpy(link->process, process);
+
+    link->next = head;
+    head = link;
+}
+
+int length() {
+    int length = 0;
+    struct block *count;
+
+    for (count = head; current != NULL; current = current->next){
+        length++;
+    }
+    return length;
+}
+
+struct block* find (char key[]) {
+    struct block* current = head;
+    if(head == NULL){
+        return NULL;
+    }
+
+    while (strcmp(current->process,key) != 0){
+        if(current->next == NULL){
+            return NULL;
+        }
+        else {
+            current = current->next;
+        }
+    }
+    return current;
+}
+
+struct block* delete(char key[]){
+    struct block* current = head;
+    struct block* previous = NULL;
+
+    if (head == NULL){
+        return NULL;
+    }
+
+    while (strcmp(current->process,key) != 0){
+        if(current->next == NULL){
+            return NULL;
+        }
+        else {
+            previous = current;
+            current = current->next;
+        }
+    }
+
+    if (current == head){
+        head = head->next;
+    }
+    else {
+        previous->next = current->next;
+    }
+    return current;
+}
+
+void bestfit(){
+
+}
+
+void firstfit(){
+
+}
+
+void worstfit(){
+
+}
+
+void nextfit(){
+
+}
+
+void request(char requestBlock[]){
+    char requestHolder[6];
+    char processName[5];
+    int memory;
+}
+
+void release(){
+
+}
+
+void listAvailable(){
+
+}
+
+void listAssigned(){
+
+}
+
+/*void find(){
+
+}*/
+
+int numberRequests(){ //to get number of blocks
+   /* if(instruction[0] == 'R' && instruction[2] == 'Q'){
+        requests++;
+    }*/
+    requests++;
+    return requests;
+}
+
+void readInstruct(char instruction[]){
+    if(instruction[0] == 'R' && instruction[2] == 'Q'){
+        numberRequests();
+        request(instruction);
+    }
+}
+
+int main(int argc, char **argv) {
+
+    char selector[8];
+    scanf("%s", selector);
+    scanf("%d", &totalMem);
+
+    FILE *fp = argc > 1 ? fopen (argv[1], "r") : stdin; //read in file given as argv[1] or read stdin
+    if (!fp){
+        fprintf(stderr, "file open failed '%s'\n",argv[1]);
+    }
+
+    int i = 0;
+
+    while(fgets(instruction, sizeof(instruction), fp) != NULL){ //fgets reads instructions, then puts them in instructArray
+        instructArray[i] = instruction;
+        i++;
+        readInstruct(instruction);
+    }
+
+    if(selector[0] == 'N'){
+        nextfit();
+    }
+
+    else if(selector[0] == 'W'){
+        worstfit();
+    }
+
+    else if (selector[0] == 'B'){
+        bestfit();
+    }
+
+    else if(selector[0] == 'F'){
+        firstfit();
+    }
+
+    else{
+        printf("Invalid Format");
+    }
+}
